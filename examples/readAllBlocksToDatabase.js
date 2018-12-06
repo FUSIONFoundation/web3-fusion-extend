15838c999fb0e55a72c1055cae92ca4d31001d5a
bret-fusion <Bret@Fusion.org>
2018-12-05 21:55:38 -0500

    starting to hook up database

--- examples/readAllBlocksToDatabase.js
+++ examples/readAllBlocksToDatabase.js
@@ -1,15 +1,89 @@
 var assert = require("assert");
 var Web3 = require("web3");
 var web3FusionExtend = require("../index.js");
+var mysql = require("promise-mysql");
 
 /*  Remember to set your environment variables to run this test
-    e.g. CONNECT_STRING="ws://3.16.110.25:9001" node ./examples/readAllBlocksToADatabase
-
-    */
+    e.g. CONNECT_STRING="ws://3.16.110.25:9001" DB_CONNECT_STRING="{'host':'localhost','user':'root','password':'password','database':'db1','connectionLimit':10}" node ./examples/readAllBlocksToADatabase
+*/
 
 console.log("CONNECT_STRING ==> ", process.env.CONNECT_STRING);
-
+console.log("DATABASE_CONNECT ==> ", process.env.DB_CONNECT_STRING);
+var dbConnect = JSON.parse(process.env.DB_CONNECT_STRING.replace(/'/g, '"'));
 var web3;
+var _pool;
+var _masterConnection;
+
+let buildTheSystem = [
+  {
+    txt: "Build Blocks",
+    sql:
+      "CREATE TABLE IF NOT EXISTS blocks (\n" +
+      "  hash VARCHAR(68) NOT NULL UNIQUE,\n" +
+      "  height BIGINT NOT NULL UNIQUE,\n" +
+      "  recCreated DATETIME DEFAULT CURRENT_TIMESTAMP,\n" +
+      "  recEdited DATETIME DEFAULT CURRENT_TIMESTAMP,\n" +
+      "  timeStamp BIGINT UNSIGNED,\n" +
+      "  numberOfTransactions int,\n" +
+      "  PRIMARY KEY (hash)\n" +
+      ") ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+  },
+  {
+    txt: "Build Transactions",
+    sql:
+      "CREATE TABLE IF NOT EXISTS transactions (\n" +
+      "  hash VARCHAR(68) NOT NULL UNIQUE,\n" +
+      "  height BIGINT NOT NULL,\n" +
+      "  recCreated DATETIME DEFAULT CURRENT_TIMESTAMP,\n" +
+      "  recEdited DATETIME DEFAULT CURRENT_TIMESTAMP,\n" +
+      "  PRIMARY KEY (hash)\n" +
+      ") ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+  }
+];
+
+var buildIndex = 0;
+
+function createTables(resolve, reject) {
+  console.log("Start - " + buildTheSystem[buildIndex].txt);
+  _masterConnection
+    .query(buildTheSystem[buildIndex].sql)
+    .then((results, fields) => {
+      buildIndex += 1;
+      if (buildIndex === buildTheSystem.length) {
+        console.log("All done building DB");
+        resolve(true);
+      } else {
+        process.nextTick(createTables);
+      }
+    })
+    .catch(err => {
+      console.log("ERROR: " + buildTheSystem[buildIndex].txt);
+      reject(err);
+    });
+}
+
+function keepSQLAlive() {
+  _pool = mysql.createPool(
+    Object.assign({ multipleStatements: true }, dbConnect)
+  );
+
+  _pool
+    .getConnection()
+    .then(conn => {
+          _masterConnection = conn;
+          return new Promise( ( resolve , reject) => {
+            createTables(resolve,reject)
+          }).then( (ret)=>{
+            _isDBConnected = true;
+            console.log("Databsase connected!"); 
+            return { success: true };
+          })
+    })
+    .catch(err => {
+      console.error("connect to database failed ", err);
+      throw err;
+    });
+}
 
 function keepWeb3Alive() {
   provider = new Web3.providers.WebsocketProvider(process.env.CONNECT_STRING);
@@ -40,6 +114,7 @@ function keepWeb3Alive() {
 // startup our web3 connection
 //
 keepWeb3Alive();
+keepSQLAlive();
 
 let lastBlock = 0;
 
