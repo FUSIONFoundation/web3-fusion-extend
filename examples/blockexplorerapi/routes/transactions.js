1921a10a14163e00a68b68c8905aa2e7814c2189
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-08-16 15:34:25 +0200

    Added JSON_EXTRACT to all query on transactions

--- examples/blockexplorerapi/routes/transactions.js
+++ examples/blockexplorerapi/routes/transactions.js
@@ -29,15 +29,15 @@ router.get("/minerForTicket/:ticketId" , function( req, res, next )  {
 // http://localhost:3000/blocks/30?sort=asc&page=200&size=50&field=timestamp
 // {"sort":"asc","page":"200","size":"50","field":"timestamp"}{"block":"30"}
 /*** examples
- * 
+ *
  *   http://localhost:3000/transactions/latest
  *   http://localhost:3000/transactions/0x346aab726aa05808698ec9aba5da4e4c4574863e87951b5107d3fdabc290bbaa
  *   http://localhost:3000/transactions/all?sort=asc&page=2&size=10&field=height
  *   fields can be:  [ timestamp, hash , type, block , asset ]
- * 
- * 
+ *
+ *
  * https://localhost:3000/transactions/all?sort=desc&page=0&size=20&field=height&returnTickets=onlytickets
- * 
+ *
  */
 router.get("/:hash", function(req, res, next) {
   let allowedFields = {
@@ -125,10 +125,11 @@ router.get("/:hash", function(req, res, next) {
   }
 
   if (hash === "all") {
+    console.log(req.query.address);
     if ( req.query.address  ) {
-      getConnection().then(conn => {
+        getConnection().then(conn => {
         conn
-          .query(`SELECT * FROM transactions where (toAddress=? or commandExtra3 = ? or fromAddress=?) ${tickreturns} order by ${field} ${sort} limit ?,?` , [ req.query.address, req.query.address,  req.query.address, (index>=0 ? index : page*size), size ] )
+          .query(`SELECT * FROM transactions where (toAddress=? or commandExtra3 = ? or fromAddress= ? or JSON_EXTRACT(receipt, '$.from') = '${req.query.address}' or JSON_EXTRACT(receipt, '$.to') = '${req.query.address}') ${tickreturns} order by ${field} ${sort} limit ?,?` , [ req.query.address, req.query.address,  req.query.address, (index>=0 ? index : page*size), size ])
           .then(rows => {
             res.json(rows)
           })
